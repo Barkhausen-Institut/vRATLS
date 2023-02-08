f3afddf785c5a1439f53651ab73c3c8cced93d44
Sebastian Ertel <sebastian.ertel@gmail.com>
2023-02-08 20:47:34 +0100

    Nix and dune build setup.

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 A formal specification for RATLS (remote attestation embedded into TLS).
 
 We are using [SSProve](https://github.com/SSProve), a framework for security proofs in Coq.
+
+## Setup
+
+Do not forget to clone this project with recursive submodules:
+```git clone --recurse-submodules```
+
+Please use [`nix`](https://nixos.org/download.html) to get all the setup you need for this project.
+Please [enable `flake` support in `nix`](https://nixos.wiki/wiki/Flakes). 
+If the `~/.config/nix/` folder and/or the `~/.config/nix/nix.conf` file do not exist then just create them.
+
+After that all you need to issue at the command line is:
+```
+nix develop
+```
+Be prepared to wait a while because it will essentially build everything from scratch, even Coq itself.
+Once it finished, the SSProve was successfully compiled and you are all set.
+
+## Development
+
+We use `dune` for building the project.
+So please run:
+```dune build```
--- flake.nix
+++ flake.nix
@@ -7,43 +7,74 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        spacemacs = pkgs.stdenv.mkDerivation {
-          name = "spacemacs";
-          src = pkgs.fetchFromGitHub {
-            owner = "syl20bnr";
-            repo = "spacemacs";
-            rev = "58a52ae6ef6c573bb9731cfcf1b9fe95627b2cc9";
-            hash = "sha256-5wP+EH/gs0zfzt/ahKEM294o5udTmG+hHDFjpV2oM3w=";
-          };
-          installPhase = "mkdir $out ; mv * .??* $out/";
+#        spacemacs = pkgs.stdenv.mkDerivation {
+#          name = "spacemacs";
+#          src = pkgs.fetchFromGitHub {
+#            owner = "syl20bnr";
+#            repo = "spacemacs";
+#            rev = "58a52ae6ef6c573bb9731cfcf1b9fe95627b2cc9";
+#            hash = "sha256-5wP+EH/gs0zfzt/ahKEM294o5udTmG+hHDFjpV2oM3w=";
+#          };
+#          installPhase = "mkdir $out ; mv * .??* $out/";
+#        };
+
+	      #ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_09;
+
+        coq = pkgs.coq_8_14.override {
+          #customOCamlPackages = ocamlPackages;
         };
-
-	      ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_09;
-
-        coq = pkgs.coq_8_14.override { customOCamlPackages = ocamlPackages; };
+        # See here: https://github.com/vellvm/vellvm/blob/dev/flake.nix
+        coqPackages = pkgs.coqPackages_8_14.overrideScope'
+          (self: super: {
+            equations          = super.equations.override { version = "1.3+8.14"; };
+            mathcomp           = super.mathcomp.override { version = "1.13.0"; };
+            mathcomp-ssreflect = super.mathcomp-ssreflect.override { version = "1.13.0"; };
+            mathcomp-analysis  = super.mathcomp-analysis.override { version = "0.3.13"; };
+            extructures        = super.extructures.override { version = "0.3.1"; };
+            deriving           = super.deriving.override { version = "0.1"; };
+         });
       in {
-        defaultDevShell = pkgs.mkShell {
-          packages = 
-            # basic tools 
-            (with pkgs; [ coq ])
+        devShell = pkgs.mkShell {
+          packages =
+            # basic tools
+            (with pkgs; [ coq emacs gnumake ])
             ++
-            (with ocamlPackages; [
-              ocaml dune_2 findlib base core stdio parsexp hashcons zarith
-            ]);
-          
+#            (with ocamlPackages; [
+#              ocaml dune_2 findlib base core stdio parsexp hashcons zarith
+#            ])
+#            ++
+            (with coqPackages; [
+              equations
+              mathcomp-analysis
+              mathcomp-ssreflect
+              extructures
+              deriving
+              # This did not work.
+                      #equations.override { version = "1.3"; }
+                      #mathcomp-analysis.override { version = "0.3.13"; }
+                      #mathcomp-ssreflect.override { version = "1.13.0"; }
+                      #extructures.override { version = "0.3.1"; }
+                      #deriving.override { version = "0.1"; }
+                      ]
+            );
+
           shellHook = ''
-            export XDG_CONFIG_HOME=$PWD/.config
-            export SPACEMACSDIR=$XDG_CONFIG_HOME/spacemacs
-
-            if ! test -d "$XDG_CONFIG_HOME/emacs" ; then
-              echo 'Installing spacemacs.'
-              mkdir -p "$XDG_CONFIG_HOME"
-              cp -Rp ${spacemacs} "$XDG_CONFIG_HOME/emacs"
-              chmod -R u+w "$XDG_CONFIG_HOME/emacs"
-            fi
-
-            test -r ~/.shellrc && . ~/.shellrc
-          '';
+                    alias ll="ls -lasi"
+                    cd ssprove && make clean && make
+                    '';
+#          shellHook = ''
+#            export XDG_CONFIG_HOME=$PWD/.config
+#            export SPACEMACSDIR=$XDG_CONFIG_HOME/spacemacs
+#
+#            if ! test -d "$XDG_CONFIG_HOME/emacs" ; then
+#              echo 'Installing spacemacs.'
+#              mkdir -p "$XDG_CONFIG_HOME"
+#              cp -Rp ${spacemacs} "$XDG_CONFIG_HOME/emacs"
+#              chmod -R u+w "$XDG_CONFIG_HOME/emacs"
+#            fi
+#
+#            test -r ~/.shellrc && . ~/.shellrc
+#          '';
         };
       }
     );
