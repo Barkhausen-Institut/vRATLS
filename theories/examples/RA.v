f95f1d782d8d0e0d0d4372ee1a13a10b084f1af5
Sebastian Ertel <sebastian.ertel@gmail.com>
2023-10-03 00:13:39 +0200

    substantial rewrite. but it does not work yet.

--- theories/examples/RA.v
+++ theories/examples/RA.v
@@ -66,10 +66,22 @@ Here we will look at the remote attestation that is using a TPM for secure hardw
 cryptography. It is like the version used on the RATLS paper.
 *)
 
-Module Type SignatureParams.
+#[local] Open Scope package_scope.
 
-    Variable (n: nat).
-    Definition pos_n: nat := 2^n.
+Variable (n: nat).
+Definition pos_n: nat := 2^n.
+
+(**
+  We can't use sets directly in [choice_type] so instead we use a map to units.
+  We can then use [domm] to get the domain, which is a set.
+ *)
+Definition chSet t := chMap t 'unit.
+Notation " 'set t " := (chSet t) (in custom pack_type at level 2).
+Notation " 'set t " := (chSet t) (at level 2): package_scope.
+
+Definition tt := Datatypes.tt.
+
+Module Type SignatureParams.
 
     (*
       FIXME This does not make much sense, does it?
@@ -88,22 +100,28 @@ Module Type SignatureParams.
 
     Definition SecKey : choice_type := chFin(mkpos pos_n).
     Definition PubKey : choice_type := chFin(mkpos pos_n).
-    Definition State : choice_type := chFin(mkpos pos_n).
-    Definition Challenge : choice_type := chFin(mkpos pos_n).
-    Definition Attestation : choice_type := chFin(mkpos pos_n).
-    Definition Message : choice_type := chFin(mkpos pos_n).
     Definition Signature : choice_type := chFin(mkpos pos_n).
 
 End SignatureParams.
 
+Module Type SignatureConstraints.
+  Parameter Message : finType.
+End SignatureConstraints.
+
 (** |  SIGNATURE  |
     |   SCHEME    | **)
 
-Module Type SignatureAlgorithms (π : SignatureParams).
+Module Type SignatureAlgorithms (π1 : SignatureParams) (π2 : SignatureConstraints).
 
-  Import π.
+  Import π1.
+  Import π2.
 
-  #[local] Open Scope package_scope.
+  (* #[local] Open Scope package_scope. *)
+
+  (* FIXME THis is broken. It creates the space [I_(pos_n)]. *)
+  Parameter Message_pos : Positive #|Message|.
+  #[local] Existing Instance Message_pos.
+  Definition chMessage := 'fin #|Message|.
 
   (*
     FIXME
@@ -120,238 +138,51 @@ Module Type SignatureAlgorithms (π : SignatureParams).
     S: From the presence of a public-secret key pair.
    *)
 
-  (*TODO Use the [kgen] function from MACCA. *)
+  (*TODO Use the [kgen] function from MACCA.
+    Also check out AsymmScheme on how to define it in a module type.
+   *)
   Parameter KeyGen : (SecKey × PubKey).
 
-  Parameter Sign : ∀ {M:Type} (sk : SecKey) (m : M), Signature.
+  Parameter Sign : ∀ (sk : SecKey) (m : chMessage), Signature.
 
-  Parameter Ver_sig : ∀ (pk : PubKey) (sig : Signature) (m : Message), 'bool.
-
-  Parameter Attest :
-    ∀ {L : {fset Location}} (sk : SecKey) ( c : Challenge ) (s : State),
-       code L [interface] Signature.
-
-  Parameter Ver_att :
-    ∀ {L : {fset Location}} (pk : PubKey) (att : Attestation)
-                   ( c : Challenge) ( s : State),
-       code L [interface] 'bool.
+  Parameter Ver_sig : ∀ (pk : PubKey) (sig : Signature) (m : chMessage), 'bool.
 
 End SignatureAlgorithms.
 
-Module RemoteAttestation (π : SignatureParams)
-  (Alg : SignatureAlgorithms π).
+Module Type SignatureScheme
+  (π1 : SignatureParams)
+  (π2 : SignatureConstraints)
+  (Alg : SignatureAlgorithms π1 π2).
 
-  Import π.
+  Import π1.
+  Import π2.
   Import Alg.
 
-  #[local] Open Scope package_scope.
+  (* #[local] Open Scope package_scope. *)
 
   Notation " 'pubkey "    := PubKey      (in custom pack_type at level 2).
   Notation " 'pubkey "    := PubKey      (at level 2): package_scope.
   Notation " 'signature " := Signature   (in custom pack_type at level 2).
   Notation " 'signature " := Signature   (at level 2): package_scope.
-  Notation " 'state "     := State       (in custom pack_type at level 2).
-  Notation " 'state "     := State       (at level 2): package_scope.
-  Notation " 'challenge " := Challenge   (in custom pack_type at level 2).
-  Notation " 'challenge " := Challenge   (at level 2): package_scope.
-  Notation " 'message "   := Message     (in custom pack_type at level 2).
-  Notation " 'message "   := Message     (at level 2): package_scope.
-  Notation " 'att "       := Signature (in custom pack_type at level 2).
-  Notation " 'att "       := Signature (at level 2): package_scope.
-
-  (**
-  We can't use sets directly in [choice_type] so instead we use a map to units.
-  We can then use [domm] to get the domain, which is a set.
-  *)
-  Definition chSet t := chMap t 'unit.
-  Notation " 'set t " := (chSet t) (in custom pack_type at level 2).
-  Notation " 'set t " := (chSet t) (at level 2): package_scope.
-
-  Definition tt := Datatypes.tt.
+  Notation " 'message "   := chMessage     (in custom pack_type at level 2).
+  Notation " 'message "   := chMessage     (at level 2): package_scope.
 
   Definition pk_loc      : Location := (PubKey    ; 0%N).
   Definition sk_loc      : Location := (SecKey    ; 1%N).
-  Definition message_loc : Location := (Message   ; 2%N).
-  Definition sign_loc    : Location := ('set ('signature × 'message); 3%N).
-  Definition state_loc   : Location := (State    ; 4%N).
-  Definition chal_loc    : Location := (Challenge ; 5%N).
-  Definition attest_loc  : Location := ('set ('att × 'challenge × 'state ) ; 6%N).
+  Definition sign_loc    : Location := ('set ('signature × 'message); 2%N).
 
   Definition get_pk    : nat := 42. (* routine to get the public key *)
-  Definition get_state : nat := 43. (* routine to get the state to be attested *)
   Definition sign      : nat := 44. (* routine to sign a message *)
   Definition verify_sig: nat := 45. (* routine to verify the signature *)
-  Definition verify_att: nat := 46.
-
-  Notation " 'attest "    := Attestation    (in custom pack_type at level 2).
-  Definition attest    : nat := 47. (* routine to attest *)
-
-  (*
-TODO:
-    Definition Attestation_locs := fset [:: pk_loc ; sk_loc; sign_loc ].
-
-
-    Definition hash ((chal,st):'set ('challenge × 'state )) : 'set ('message) :=
-      (chal,st).
-  *)
-(*
-TODO remove below
- *)
-
-  Equations hash {c s} (x: choice_type) (H: x = chMap (chProd (chFin c) (chFin s)) 'unit) : choice_type :=
-    hash (chMap (chProd (chFin cPos) (chFin sPos)) _) eq_refl :=
-      chMap
-        (chFin
-           (mkpos
-              (pos cPos * pos sPos)
-              (* (Positive_prod (cond_pos cPos) (cond_pos sPos)) *)
-        ))
-        'unit.
-
-  Check hash_equation_1.
-  Check FunctionalElimination_hash.
-
-Fail  Equations hash' (x: choice_type) {H: x = 'set ('challenge × 'state)} : choice_type :=
-    hash' (chMap (chProd (chFin cPos) (chFin sPos)) _) eq_refl :=
-      chMap
-        (chFin
-           (mkpos
-              (pos cPos * pos sPos)
-              (* (Positive_prod (cond_pos cPos) (cond_pos sPos)) *)
-        ))
-        'unit.
-
-  Lemma hash_spec: hash ('set ('challenge × 'state)) eq_refl = 'set ('message).
-  Proof.
-    rewrite /chSet/Challenge/State/Message.
-    set x := chMap 'fin pos_n 'unit.
-    elim/FunctionalElimination_hash: (hash (chMap ('fin pos_n × 'fin pos_n) 'unit) erefl) (* (hash _ _) *).
-    move => c s.
-    rewrite /x.
-    f_equal.
-    f_equal.
-    (*
-      This spec is just not true.
-      I believe that the problem is in the data definition.
-      It defines values instead of mathematical structure.
-     *)
-    Print Location.
-    (*
-      Note that the need for specifying a [choice_type]
-      comes from the need to store it into a heap location.
-      But the better way to achieve this, is to use a mathematical
-      structure and then create the heap locations out of this.
-      In the code, it is possible to just use the conversion
-      functions [fto] and [otf] from SSProve.
-
-      For example, in DDH we define
-      [Definition chGroup : choice_type := 'fin #|GroupSpace|.]
-      which comes directly from a mathematical structure:
-      [Definition GroupSpace : finType := FinGroup.arg_finType gT.]
-      and:
-      [Parameter gT : finGroupType.]
-
-      In RA, we do not have such a construction.
-      We essentially define:
-      [Definition Challenge : choice_type := chFin(mkpos pos_n).]
-      with:
-      [Definition pos_n: nat := 2^n.]
-      That is: there is no mathematical structure.
-      It is actually just a concrete value [2^n].
-
-      Maybe here a small explanation, why locations want to
-      be of type [choice_type]:
-      because they define default values, i.e., the initial
-      value of the heap location.
-      See [choice_type.chCannonical].
-      [choice_type] coerces to mathcomps [choiceType]
-      https://math-comp.github.io/htmldoc/mathcomp.ssreflect.choice.html
-
-      To cut a long story short:
-      A message should not be of type [choice_type] but of the
-      type of the mathematical structure, probably [I_n]
-     *)
-  Abort.
-
-  (* MACCA approach. *)
-  Context (mk_hash : 'state -> 'challenge -> 'message).
-  (* But I cannot see how the result is possible when the space is not a group.
-     That is because the heap locations in MACCA all have the same type or are subsets.
-     They never have to convert one into the other.
-   *)
-
-  (* Check this out: *)
-  Fail Definition f: 'set ('message) := chMap (chFin (mkpos 1)) chUnit.
-  Check Choice.sort 'set ('message).
-  (* So what are the inhabitants of this type?! *)
-  Fail Definition f: 'set ('message) := chMap (chFin (mkpos pos_n)) chUnit.
-  (* Nope. Check out the error message:
-     [
-Error:
-The term "chMap 'fin pos_n 'unit" has type "choice_type" while it is expected to have type
- "Choice.sort 'set ('message)".
-     ]
-     That is, it cannot reduce [Choice.sort 'set ('message)] in the type!
-     Remember [rew]?!
-     Let's check out what [Choice.sort 'set ('message)] actually should reduce to:
-   *)
-  Print Choice.sort.
-  Eval hnf in Choice.sort 'set ('message).
-  Locate FMap.
-  Check FMap.fmap_type.
-  Eval hnf in (chElement_ordType 'message).
-  Check Ordinal.
-  Check is_true.
-  Lemma two_lt_three : 2 < 3. by []. Defined.
-  Compute Ordinal two_lt_three.
-  Locate "<".
-  Definition f: 'I_3 := Ordinal two_lt_three.
-
-  Definition X: choice_type := (chFin (mkpos 3)).
-  Definition g: X := Ordinal two_lt_three.
-
-  Locate "\in".
-  Check in_mem.
-  Locate in_mem.
-  Print Coq.ssr.ssrbool.
-
-  (* We still need a group in order to perform any
-     operation on two elements.
-   *)
-  
-
-  Parameter Hash :
-    	State -> Challenge ->
-      Message.
-(*
-  Parameter Hash_refl :
-    forall s1 c1 , Hash s1 c1 = Hash s1 c1.
-
-  Parameter Hash_bij :
-    forall s1 c1 s2 c2, s1 != s2 \/ c1 != c2  -> Hash s1 c1 != Hash s2 c2. *)
 
   (* The signature scheme requires a heap location to store the seen signatures. *)
   Definition Signature_locs_real := fset [:: pk_loc ; sk_loc].
   Definition Signature_locs_fake := Signature_locs_real :|: fset [:: sign_loc ].
 
-  (* The remote attestation protocol does the same. *)
-  Definition Attestation_locs_real := fset [:: pk_loc ; sk_loc; state_loc ].
-  Definition Attestation_locs_fake := Attestation_locs_real :|: fset [:: attest_loc ].
-
-  (*
-    The key challenge: relate [sign_loc] and [attest_loc].
-   *)
-
   Definition Sign_interface := [interface
     #val #[get_pk] : 'unit → 'pubkey ;
     #val #[sign] : 'message → 'signature ;
     #val #[verify_sig] : ('signature × 'message) → 'bool
-  ].
-
-  Definition Att_interface := [interface
-  #val #[get_pk] : 'unit → 'pubkey ;
-  #val #[attest] : 'challenge → 'signature ;
-  #val #[verify_att] : ('challenge × 'signature) → 'bool
   ].
 
   Definition Sig_real : package Signature_locs_real [interface] Sign_interface
@@ -394,7 +225,8 @@ The term "chMap 'fin pos_n 'unit" has type "choice_type" while it is expected to
       #put sk_loc := sk ;;
       let sig := Sign sk msg in
       S ← get sign_loc ;;
-      #put sign_loc := setm S (sig, msg) tt ;;
+      let S' := setm S (sig, msg) tt in
+      #put sign_loc := S' ;;
       ret sig
     };
 
@@ -409,6 +241,92 @@ The term "chMap 'fin pos_n 'unit" has type "choice_type" while it is expected to
     2,3,6: left;auto_in_fset.
     all: right; auto_in_fset.
   Defined.
+
+End SignatureScheme.
+
+Module Type RemoteAttestationParams <: SignatureConstraints.
+
+  (*
+    Internally, ['fin (mkpos pos_n] creates [ordinal of pos_n] which is [I_(pos_n)].
+    It would just be nice to find a way to derive [chState] from [State].
+   *)
+  Definition State : finType := [finType of 'I_(pos_n)].
+  Definition chState : choice_type := 'fin (mkpos pos_n). (* ordinal_choiceType (pos_n). *)
+  Definition Challenge : finType := [finType of 'I_(pos_n)].
+  Definition chChallenge : choice_type := 'fin (mkpos pos_n). (* ordinal_choiceType pos_n. *)
+  (*Definition chChallenge : choice_type := 'fin (mkpos pos_n). *)
+  Definition Attestation : choice_type := 'fin (mkpos pos_n).
+
+  Definition Message := prod_finType Challenge State.
+
+End RemoteAttestationParams.
+
+Module Type RemoteAttestationAlgorithms
+  (π1 : SignatureParams) (* TBD This is strange. The reason is because our code depends on signature scheme functions. *)
+  (π2 : RemoteAttestationParams)
+  (π3 : SignatureAlgorithms π1 π2).
+  Import π1.
+  Import π2.
+  Import π3.
+
+  Local Open Scope package_scope.
+
+  Notation " 'state "     := chState       (in custom pack_type at level 2).
+  Notation " 'state "     := chState       (at level 2): package_scope.
+  Notation " 'challenge " := chChallenge   (in custom pack_type at level 2).
+  Notation " 'challenge " := chChallenge   (at level 2): package_scope.
+
+  Definition state_loc   : Location := ('state    ; 4%N).
+  Definition attest_loc  : Location := ('set (Signature × ('challenge × 'state)) ; 5%N).
+
+  Definition verify_att: nat := 46.
+
+  Notation " 'attest "    := Attestation    (in custom pack_type at level 2).
+  Definition attest    : nat := 47. (* routine to attest *)
+
+  Parameter Hash : State -> Challenge -> chMessage.
+
+(*
+  Parameter Hash_refl :
+    forall s1 c1 , Hash s1 c1 = Hash s1 c1.
+
+  Parameter Hash_bij :
+    forall s1 c1 s2 c2, s1 != s2 \/ c1 != c2  -> Hash s1 c1 != Hash s2 c2. *)
+
+End RemoteAttestationAlgorithms.
+
+
+Module RemoteAttestation
+  (π1 : SignatureParams)
+  (π2 : RemoteAttestationParams)
+  (π3 : SignatureAlgorithms π1 π2)
+  (π4 : RemoteAttestationAlgorithms π1 π2 π3)
+  (π5 : SignatureScheme π1 π2 π3).
+
+  Import π1 π2 π3 π4 π5.
+
+
+  (* The remote attestation protocol does the same as the signature scheme, i.e.,
+     it stores the attestations handed out.
+   *)
+  Definition Attestation_locs_real := fset [:: pk_loc ; sk_loc; state_loc ].
+  Definition Attestation_locs_fake := Attestation_locs_real :|: fset [:: attest_loc ].
+
+  (*
+    The key challenge: relate [sign_loc] and [attest_loc].
+    There are 2 approaches:
+    1) we compute a [message] (Signature Scheme) from the [state] and the [challenge] of the
+       remote attestation algorithm.
+    2) we make the type of the [sign_loc] in the Signature Scheme polymorph.
+
+    This is the second approach.
+   *)
+
+  Definition Att_interface := [interface
+  #val #[get_pk] : 'unit → 'pubkey ;
+  #val #[attest] : 'challenge → 'signature ;
+  #val #[verify_att] : ('challenge × 'signature) → 'bool
+  ].
 
   Definition Att_real : package Attestation_locs_real [interface] Att_interface
   := [package
@@ -450,12 +368,12 @@ The term "chMap 'fin pos_n 'unit" has type "choice_type" while it is expected to
     #def #[attest] (chal : 'challenge) : 'attest
     {
       A ← get attest_loc ;;
-      state ← get state_loc ;;
+      s ← get state_loc ;;
       let (sk,pk) := KeyGen in
       #put pk_loc := pk ;;
       #put sk_loc := sk ;;
-      let att := Sign sk (chal, state) in
-      #put attest_loc := setm A ( att, chal, state ) tt ;;
+      let att := Sign sk (chal, s) in
+      #put attest_loc := setm A ( att, chal, s ) tt ;;
       ret att
     };
 
@@ -473,28 +391,40 @@ The term "chMap 'fin pos_n 'unit" has type "choice_type" while it is expected to
     all: left; auto_in_fset.
   Defined.
 
-  Definition Aux_locs := fset [:: sign_loc ; pk_loc ].
+  (*
+    TODO:
+    Currently, these descriptions do not facilitate an actual protocol.
+    A protocol really is where I call [attest] and [verify_att] in one single piece of code.
+    The thing that happens then is that there need to be two locations for the state:
+    one in the client and the other in the server.
+   *)
 
-  Definition Aux :
-  package Aux_locs
-  Sign_interface
-  Att_interface :=
+  (* We need a common interface, so we need to define an [AUX] for the
+     signature scheme.
+   *)
+  Definition Aux_locs := fset [:: pk_loc ; state_loc ].
+
+  Definition Aux : package Aux_locs Sign_interface Att_interface :=
   [package
     #def #[get_pk] (_ : 'unit) : 'pubkey
     {
       pk ← get pk_loc ;;
       ret pk
-    } ;
-    #def #[attest] ( '(chal,state) : ('challenge × 'state )) : 'signature
+    };
+
+    #def #[attest] ( chal : 'challenge ) : 'signature
     {
       #import {sig #[sign] : 'message  → 'signature } as sign ;;
+      state ← get state_loc ;;
       let msg := Hash state chal in
       att ← sign msg ;;
       ret att
-    } ;
-    #def #[verify_att] ('(chal, state, att) : ('challenge × 'state) × 'signature) : 'bool
+    };
+
+    #def #[verify_att] ('(chal, att) : 'challenge × 'signature) : 'bool
     {
       #import {sig #[verify_sig] : ('signature × 'message) → 'bool } as verify ;;
+      state ← get state_loc ;;
       let msg := Hash state chal in
       b  ← verify (att,msg) ;;
       ret b
@@ -509,14 +439,8 @@ The term "chMap 'fin pos_n 'unit" has type "choice_type" while it is expected to
     (t: package Lt [interface] E) (f: package Lf [interface] E): loc_GamePair E :=
     fun b => if b then {locpackage t} else {locpackage f}.
 
-  Definition Sig_unforg := @mkpair Signature_locs Signature_locs Sign_interface Sig_real Sig_ideal.
-  Definition Att_unforg := @mkpair Attestation_locs Attestation_locs Att_interface Att_real Att_ideal.
-
-(* Attestation_locs =o Aux_locs o Sig_locs
-Definition Attestation_locs := fset [:: pk_loc ; sk_loc; attest_loc ].
-
-Definition Signature_locs := fset [:: pk_loc ; sk_loc ; sign_loc ].
-Definition Aux_locs' := fset [:: sign_loc ; pk_loc ; attest_loc ]. *)
+  Definition Sig_unforg := @mkpair Signature_locs_real Signature_locs_fake Sign_interface Sig_real Sig_ideal.
+  Definition Att_unforg := @mkpair Attestation_locs_real Attestation_locs_fake Att_interface Att_real Att_ideal.
 
   Lemma sig_real_vs_att_real_true:
     Att_unforg true ≈₀  Aux ∘ Sig_unforg true.
@@ -529,13 +453,14 @@ Definition Aux_locs' := fset [:: sign_loc ; pk_loc ; attest_loc ]. *)
       move => [a1 h1] [a2 h2] [Heqa Heqh].
       intuition auto.
     - destruct x.
-      ssprove_sync_eq.
-      ssprove_sync_eq.
+      ssprove_sync_eq => state.
+      do 2! ssprove_sync_eq.
       by [apply r_ret].
     - case x => s s0.
       case s => s1 s2.
-      ssprove_sync_eq.
-      move => a.
+      ssprove_swap_lhs 0.
+      ssprove_sync_eq => state.
+      ssprove_sync_eq => pk.
       by [apply r_ret].
   Qed.
 
@@ -547,7 +472,8 @@ Definition Aux_locs' := fset [:: sign_loc ; pk_loc ; attest_loc ]. *)
     all: ssprove_code_simpl.
     - ssprove_sync_eq => pk_loc.
       by [apply r_ret].
-    - case x => challenge state.
+    - rewrite /Att_ideal.
+      case x => challenge state.
       ssprove_swap_lhs 0.
       ssprove_sync_eq.
       ssprove_swap_lhs 0.
