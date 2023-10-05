f311eebfb048af8d0ecb506add9c1ed4c9e6da7f
Sebastian Ertel <sebastian.ertel@gmail.com>
2023-10-05 22:38:30 +0200

    switching to a simpler approach. indistignuishability proof for ideal parts.

--- theories/examples/RA.v
+++ theories/examples/RA.v
@@ -29,6 +29,13 @@ Require Import List.
 
 Set Equations With UIP.
 
+(*
+  This is needed to make definitions with Equations transparent.
+  Otherwise they are opaque and code simplifications in the
+  proofs with [ssprove_code_simpl] does not resolve properly.
+ *)
+Set Equations Transparent.
+
 Set Bullet Behavior "Strict Subproofs".
 Set Default Goal Selector "!".
 Set Primitive Projections.
@@ -105,8 +112,22 @@ Module Type SignatureParams.
 End SignatureParams.
 
 Module Type SignatureConstraints.
-  Parameter Message : finType.
+  Definition chMessage : choice_type := 'fin (mkpos pos_n).
 End SignatureConstraints.
+
+Module Type SignatureConstraintsFail.
+  Parameter Message : finType.
+
+  (* FIXME THis is broken.
+     It creates the space [I_(pos_n)].
+     But that might be because I chose the wrong message type!
+     Maybe give it another try.
+   *)
+  Parameter Message_pos : Positive #|Message|.
+  #[local] Existing Instance Message_pos.
+  Definition chMessage := 'fin #|Message|.
+
+End SignatureConstraintsFail.
 
 (** |  SIGNATURE  |
     |   SCHEME    | **)
@@ -115,13 +136,6 @@ Module Type SignatureAlgorithms (π1 : SignatureParams) (π2 : SignatureConstrai
 
   Import π1.
   Import π2.
-
-  (* #[local] Open Scope package_scope. *)
-
-  (* FIXME THis is broken. It creates the space [I_(pos_n)]. *)
-  Parameter Message_pos : Positive #|Message|.
-  #[local] Existing Instance Message_pos.
-  Definition chMessage := 'fin #|Message|.
 
   (*
     FIXME
@@ -244,7 +258,11 @@ Module Type SignatureScheme
 
 End SignatureScheme.
 
-Module Type RemoteAttestationParams <: SignatureConstraints.
+(* Sadly, this approach being polymorphic about the heap location
+   did not work out.
+   I was unable to create a message of type [choice_type] in the RA part.
+ *)
+Module Type RemoteAttestationParamsFail <: SignatureConstraintsFail.
 
   (*
     Internally, ['fin (mkpos pos_n] creates [ordinal of pos_n] which is [I_(pos_n)].
@@ -258,10 +276,270 @@ Module Type RemoteAttestationParams <: SignatureConstraints.
   Definition Attestation : choice_type := 'fin (mkpos pos_n).
 
   Definition Message := prod_finType Challenge State.
+  #[export] Instance Message_pos : Positive #|Message|.
+  Admitted.
+  Definition chMessage := 'fin #|Message|.
 
-End RemoteAttestationParams.
+End RemoteAttestationParamsFail.
 
-Module Type RemoteAttestationAlgorithms
+Module Type RemoteAttestationParamsHeapHash <: SignatureConstraints.
+
+  Definition chState : choice_type := 'fin (mkpos pos_n).
+  Definition chChallenge : choice_type := 'fin (mkpos pos_n).
+  Definition Attestation : choice_type := 'fin (mkpos pos_n).
+
+  Definition chMessage := 'fin (mkpos pos_n).
+
+End RemoteAttestationParamsHeapHash.
+
+Module Type RemoteAttestationAlgorithmsHeapHash
+  (π1 : SignatureParams) (* TBD This is strange. The reason is because our code depends on signature scheme functions. *)
+  (π2 : RemoteAttestationParamsHeapHash)
+  (π3 : SignatureAlgorithms π1 π2).
+  Import π1.
+  Import π2.
+  Import π3.
+
+  Local Open Scope package_scope.
+
+  Notation " 'state "     := chState       (in custom pack_type at level 2).
+  Notation " 'state "     := chState       (at level 2): package_scope.
+  Notation " 'challenge " := chChallenge   (in custom pack_type at level 2).
+  Notation " 'challenge " := chChallenge   (at level 2): package_scope.
+
+  Definition state_loc   : Location := ('state    ; 4%N).
+  Definition attest_loc  : Location := ('set (Signature × chMessage) ; 2%N).
+
+  Definition verify_att: nat := 46.
+
+  Notation " 'attest "    := Attestation    (in custom pack_type at level 2).
+  Definition attest    : nat := 47. (* routine to attest *)
+
+  (*
+    FIXME this hash function spec is insufficient.
+    It allows to discard one of the arguments and just return the other as message!
+   *)
+  Parameter Hash : chState -> chChallenge -> chMessage.
+
+(*
+  Parameter Hash_refl :
+    forall s1 c1 , Hash s1 c1 = Hash s1 c1.
+
+  Parameter Hash_bij :
+    forall s1 c1 s2 c2, s1 != s2 \/ c1 != c2  -> Hash s1 c1 != Hash s2 c2. *)
+
+End RemoteAttestationAlgorithmsHeapHash.
+
+
+Module RemoteAttestationHeapSig
+  (π1 : SignatureParams)
+  (π2 : RemoteAttestationParamsHeapHash)
+  (π3 : SignatureAlgorithms π1 π2)
+  (π4 : RemoteAttestationAlgorithmsHeapHash π1 π2 π3)
+  (π5 : SignatureScheme π1 π2 π3).
+
+  Import π1 π2 π3 π4 π5.
+
+
+  (* The remote attestation protocol does the same as the signature scheme, i.e.,
+     it stores the attestations handed out.
+   *)
+  Definition Attestation_locs_real := fset [:: pk_loc ; sk_loc; state_loc ].
+  Definition Attestation_locs_fake := Attestation_locs_real :|: fset [:: attest_loc ].
+
+  (*
+    The key challenge: relate [sign_loc] and [attest_loc].
+    There are 2 approaches:
+    1) we compute a [message] (Signature Scheme) from the [state] and the [challenge] of the
+       remote attestation algorithm.
+    2) we make the type of the [sign_loc] in the Signature Scheme polymorph.
+
+    This is the second approach.
+   *)
+
+  Definition Att_interface := [interface
+  #val #[get_pk] : 'unit → 'pubkey ;
+  #val #[attest] : 'challenge → 'signature ;
+  #val #[verify_att] : ('challenge × 'signature) → 'bool
+  ].
+
+  Definition Att_real : package Attestation_locs_real [interface] Att_interface
+  := [package
+    #def  #[get_pk] (_ : 'unit) : 'pubkey
+    {
+      pk ← get pk_loc  ;;
+      ret pk
+    };
+
+    #def #[attest] (chal : 'challenge) : 'signature
+    {
+      state ← get state_loc ;;
+      let (sk,pk) := KeyGen in
+      #put pk_loc := pk ;;
+      #put sk_loc := sk ;;
+      let msg := Hash state chal in
+      let att := Sign sk msg in
+      ret att
+    };
+
+    #def #[verify_att] ('(chal, att) : ('challenge × 'signature)) : 'bool
+    {
+      pk ← get pk_loc ;;
+      state ← get state_loc ;;
+      let msg := Hash state chal in
+      let bool := Ver_sig pk att msg in
+      ret bool
+    }
+  ].
+
+  Equations Att_ideal : package Attestation_locs_fake [interface] Att_interface :=
+  Att_ideal := [package
+    #def  #[get_pk] (_ : 'unit) : 'pubkey
+    {
+      pk ← get pk_loc ;;
+      ret pk
+    };
+
+    #def #[attest] (chal : 'challenge) : 'attest
+    {
+      A ← get attest_loc ;;
+      s ← get state_loc ;;
+      let (sk,pk) := KeyGen in
+      #put pk_loc := pk ;;
+      #put sk_loc := sk ;;
+      let msg := Hash s chal in
+      let att := Sign sk msg in
+      #put attest_loc := setm A ( att, msg ) tt ;;
+      ret att
+    };
+
+    #def #[verify_att] ('(chal, att) : ('challenge × 'attest)) : 'bool
+    {
+      A ← get attest_loc ;;
+      state ← get state_loc ;;
+      let msg := Hash state chal in
+      let b :=  (att, msg) \in domm A in
+      ret b
+    }
+  ].
+  Next Obligation.
+    ssprove_valid; rewrite /Attestation_locs_fake/Attestation_locs_real in_fsetU; apply /orP.
+    1,3,7: right;auto_in_fset.
+    all: left; auto_in_fset.
+  Defined.
+
+
+  (*
+    TODO:
+    Currently, these descriptions do not facilitate an actual protocol.
+    A protocol really is where I call [attest] and [verify_att] in one single piece of code.
+    The thing that happens then is that there need to be two locations for the state:
+    one in the client and the other in the server.
+   *)
+
+  (* We need a common interface, so we need to define an [AUX] for the
+     signature scheme.
+   *)
+  Definition Aux_locs := fset [:: pk_loc ; state_loc ].
+
+  Definition Aux : package Aux_locs Sign_interface Att_interface :=
+  [package
+    #def #[get_pk] (_ : 'unit) : 'pubkey
+    {
+      pk ← get pk_loc ;;
+      ret pk
+    };
+
+    #def #[attest] ( chal : 'challenge ) : 'signature
+    {
+      #import {sig #[sign] : 'message  → 'signature } as sign ;;
+      state ← get state_loc ;;
+      let msg := Hash state chal in
+      att ← sign msg ;;
+      ret att
+    };
+
+    #def #[verify_att] ('(chal, att) : 'challenge × 'signature) : 'bool
+    {
+      #import {sig #[verify_sig] : ('signature × 'message) → 'bool } as verify ;;
+      state ← get state_loc ;;
+      let msg := Hash state chal in
+      b  ← verify (att,msg) ;;
+      ret b
+      (* When I just write:
+         [verify (att,msg)]
+         Then SSProve errors out and cannot validate the package. Why?
+       *)
+    }
+  ].
+
+  Definition mkpair {Lt Lf E}
+    (t: package Lt [interface] E) (f: package Lf [interface] E): loc_GamePair E :=
+    fun b => if b then {locpackage t} else {locpackage f}.
+
+  Definition Sig_unforg := @mkpair Signature_locs_real Signature_locs_fake Sign_interface Sig_real Sig_ideal.
+  Definition Att_unforg := @mkpair Attestation_locs_real Attestation_locs_fake Att_interface Att_real Att_ideal.
+
+  Lemma sig_real_vs_att_real_true:
+    Att_unforg true ≈₀  Aux ∘ Sig_unforg true.
+  Proof.
+    eapply eq_rel_perf_ind_eq.
+    simplify_eq_rel x.
+    all: ssprove_code_simpl.
+    - eapply rpost_weaken_rule.
+      1: eapply rreflexivity_rule.
+      move => [a1 h1] [a2 h2] [Heqa Heqh].
+      intuition auto.
+    - destruct x.
+      ssprove_sync_eq => state.
+      do 2! ssprove_sync_eq.
+      by [apply r_ret].
+    - case x => s s0.
+      case s => s1 s2.
+      ssprove_swap_lhs 0.
+      ssprove_sync_eq => state.
+      ssprove_sync_eq => pk.
+      by [apply r_ret].
+  Qed.
+
+  Lemma sig_ideal_vs_att_ideal_false :
+    Att_unforg false ≈₀ Aux ∘ Sig_unforg false.
+  Proof.
+    eapply eq_rel_perf_ind_eq.
+    simplify_eq_rel x.
+    all: ssprove_code_simpl.
+    - ssprove_sync_eq => pk_loc.
+      by [apply r_ret].
+    - ssprove_swap_lhs 0; ssprove_sync_eq => state.
+      do 2! (ssprove_swap_lhs 0; ssprove_sync_eq).
+      ssprove_sync_eq => sig.
+      ssprove_sync_eq.
+      by [apply r_ret].
+    - case x => a b.
+      ssprove_swap_lhs 0; ssprove_sync_eq => state.
+      ssprove_sync_eq => sig.
+      by [apply r_ret].
+  Qed.
+
+  (* This is what the theorem is supposed to look like, but it doesn't compile! -> to be changed*)
+  Theorem RA_unforg LA A :
+    ∀ LA A,
+      ValidPackage LA [interface
+                       #val #[get_pk] : 'unit → 'pubkey ;
+                       #val #[sign] : ('challenge × 'state) → 'attest ;
+                       #val #[verify_sig] : ( ('challenge × 'state) × 'attest) → 'bool
+        ] A_export A →
+      fdisjoint LA (Sig_unforg true).(locs) →
+      fdisjoint LA (Sig_unforg false).(locs) →
+      fdisjoint LA Aux_locs →
+      fdisjoint LA (Att_unforg true).(locs) →
+      fdisjoint LA (Att_unforg false).(locs) →
+      Advantage Att_unforg A <= AdvantageE Sig_real Sig_ideal (A ∘ Aux).
+  Proof.
+
+End RemoteAttestationAlgorithmsHeapSig.
+
+Module Type RemoteAttestationAlgorithmsNoHeapSig
   (π1 : SignatureParams) (* TBD This is strange. The reason is because our code depends on signature scheme functions. *)
   (π2 : RemoteAttestationParams)
   (π3 : SignatureAlgorithms π1 π2).
@@ -284,6 +562,10 @@ Module Type RemoteAttestationAlgorithms
   Notation " 'attest "    := Attestation    (in custom pack_type at level 2).
   Definition attest    : nat := 47. (* routine to attest *)
 
+  (*
+    FIXME this hash function spec is insufficient.
+    It allows to discard one of the arguments and just return the other as message!
+   *)
   Parameter Hash : State -> Challenge -> chMessage.
 
 (*
@@ -293,14 +575,14 @@ Module Type RemoteAttestationAlgorithms
   Parameter Hash_bij :
     forall s1 c1 s2 c2, s1 != s2 \/ c1 != c2  -> Hash s1 c1 != Hash s2 c2. *)
 
-End RemoteAttestationAlgorithms.
+End RemoteAttestationAlgorithmsNoHeapSig.
 
 
-Module RemoteAttestation
+Module RemoteAttestationNoHeapSig
   (π1 : SignatureParams)
   (π2 : RemoteAttestationParams)
   (π3 : SignatureAlgorithms π1 π2)
-  (π4 : RemoteAttestationAlgorithms π1 π2 π3)
+  (π4 : RemoteAttestationAlgorithmsNoHeapSig π1 π2 π3)
   (π5 : SignatureScheme π1 π2 π3).
 
   Import π1 π2 π3 π4 π5.
@@ -464,6 +746,18 @@ Module RemoteAttestation
       by [apply r_ret].
   Qed.
 
+  (*
+    Another invariant over the heaps:
+    [f(h__attest) = h__sign]
+    with
+    [
+      Definition f (h) : h :=
+      match h__loc with
+      | attest_loc (chal, state) att => hash (chal,state)
+      | _ => id
+    ]
+   *)
+
   Lemma sig_ideal_vs_att_ideal_false :
   Att_unforg false ≈₀ Aux ∘ Sig_unforg false.
   Proof.
@@ -495,3 +789,4 @@ Module RemoteAttestation
     Advantage Att_unforg <= Advantage Sig_unforg.
 Proof.
   
+End RemoteAttestationNoHeapSig.
