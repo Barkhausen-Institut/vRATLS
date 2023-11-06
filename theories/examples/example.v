b0ab2228533cb972a723aca9f3f798a6d43b633b
JaMaeh <jannik.maehn@barkhauseninstitut.org>
2023-11-06 17:00:42 +0100

    example written, but it need some more steps in the proof

--- theories/examples/example.v
+++ theories/examples/example.v
@@ -14,162 +14,76 @@ From Crypt Require Import Axioms ChoiceAsOrd SubDistr Couplings
 From Coq Require Import Utf8.
 From extructures Require Import ord fset fmap.
 
+From Equations Require Import Equations.
+Require Equations.Prop.DepElim.
 Require Import Coq.Init.Logic.
 Require Import List.
 
-Set Bullet Behavior "Strict Subproofs".
-Set Default Goal Selector "!".
-Set Primitive Projections.
 
 Import PackageNotation.
 
-Module Type SignatureDefs.
+Obligation Tactic := idtac.
 
-    Variable (n: nat).
-    Definition pos_n: nat := 2^n.
-    Definition Seed : choice_type := chFin(mkpos pos_n).
-    Definition SecKey : choice_type := chFin(mkpos pos_n).
-    Definition PubKey : choice_type := chFin(mkpos pos_n).
+#[local] Open Scope package_scope.
 
-    Parameter KeyGen : Seed -> (SecKey × PubKey).
+Variable (n: nat).
+Definition pos_n: nat := 2^n.
+Definition tt := Datatypes.tt.
 
-    #[local] Open Scope package_scope.
-    Parameter KeyGenM :
-      ∀ {L : {fset Location}} (sd : Seed),
-        code L [interface] (SecKey × PubKey).
 
-End SignatureDefs.
+Definition SecKey : choice_type := chFin(mkpos pos_n).
+Definition PubKey : choice_type := chFin(mkpos pos_n).
 
-Module RemoteAttestation (π : SignatureDefs).
+Parameter KeyGen : ∀ {L : {fset Location}},
+  code L [interface] (SecKey × PubKey).
 
-  Import π.
+Notation " 'pk "    := PubKey      (in custom pack_type at level 2).
+Notation " 'pk "    := PubKey      (at level 2): package_scope.
+Notation " 'sk "    := SecKey      (in custom pack_type at level 2).
+Notation " 'sk "    := SecKey      (at level 2): package_scope.
 
-  Notation " 'pubkey "    := PubKey      (in custom pack_type at level 2) : package_scope.
-  Notation " 'seckey "    := SecKey      (in custom pack_type at level 2) : package_scope.
-  Notation " 'seed "      := Seed        (in custom pack_type at level 2) : package_scope.
+Definition keygen : nat := 1.
 
-  Definition pk_loc      : Location := (PubKey    ; 0%N).
-  Definition sk_loc      : Location := (SecKey    ; 1%N).
-  Definition key_gen   : nat := 42.
-  Definition KeyGen_locs := fset [:: pk_loc ; sk_loc ].
-  Definition KG_interface := [interface #val #[key_gen] : 'seed → 'pubkey ].
+Definition kg_int := [interface
+  #val #[keygen] : 'unit → 'pk
+].
 
-  Definition KG_real :
-  package KeyGen_locs
-    [interface]
-    KG_interface
-  :=
-  [package
-    #def  #[key_gen] (sd : 'seed) : 'pubkey
-    {
-    let (sk,pk) := KeyGen sd in
-    ret pk
-    }
-  ].
+Definition KG_lhs : package (fset [:: ]) [interface] kg_int
+ := [package
+  #def #[keygen] (_ : 'unit) : 'pk
+   {
+     '(sk,pk) ← KeyGen ;;
+      ret pk
+   }
+].
 
-  Definition KG_ideal :
-  package KeyGen_locs
-    [interface]
-    KG_interface
-  :=
-  [package
-    #def  #[key_gen] (sd : 'seed) : 'pubkey
-    {
-    let (sk,pk) := KeyGen sd in
-    ret pk
-    }
-  ].
+Definition KG_rhs : package (fset [:: ]) [interface] kg_int
+ := [package
+  #def #[keygen] (_ : 'unit) : 'pk
+   {
+     '(sk,pk) ← KeyGen ;;
+      ret pk
+   }
+].
 
-  Definition mkpair {Lt Lf E}
-    (t: package Lt [interface] E)
-    (f: package Lf [interface] E):
-    loc_GamePair E :=
-    fun b => if b then {locpackage t} else {locpackage f}.
+Definition Aux : package (fset [:: ]) kg_int kg_int
+ := [package
+  #def #[keygen] (_ : 'unit) : 'pk
+   {
+     #import {sig #[keygen] : 'unit  → 'sk } as kg ;;
+      pk ← kg tt ;;
+      ret pk
+   }
+].
 
-  Definition KG_ind := @mkpair KeyGen_locs KeyGen_locs KG_interface KG_real KG_ideal.
+Lemma concat:
+  KG_lhs ≈₀  Aux ∘ KG_rhs.
+Proof.
+  eapply eq_rel_perf_ind_eq.
+  simplify_eq_rel x.
+  ssprove_code_simpl.
 
-  Lemma KG_real_vs_KG_true :
-  KG_ind true ≈₀ KG_ind false.
-  Proof.
-    eapply eq_rel_perf_ind_eq.
-    simplify_eq_rel x.
-    all: ssprove_code_simpl.
-    Fail eapply rreflexivity_rule.
-    Fail ssprove_sync.
-    Fail eapply r_ret.
-    Check rpost_weaken_rule.
-    eapply rpost_weaken_rule.
-    1:{ eapply rreflexivity_rule. }
-    intros.
-    move: H1; case eq_a0: a₀ => [b₀ s₀]; move => eq_a1 //=.
-    move: eq_a1; case eq_a1: a₁ => [b₁ s₁]; move => eq_b_s.
-    by [injection eq_b_s => eq_s eq_b].
-  Qed.
 
-End RemoteAttestation.
 
-Module RemoteAttestationM (π : SignatureDefs).
 
-  Import π.
 
-  Notation " 'pubkey "    := PubKey      (in custom pack_type at level 2) : package_scope.
-  Notation " 'seckey "    := SecKey      (in custom pack_type at level 2) : package_scope.
-  Notation " 'seed "      := Seed        (in custom pack_type at level 2) : package_scope.
-
-  Definition pk_loc      : Location := (PubKey    ; 0%N).
-  Definition sk_loc      : Location := (SecKey    ; 1%N).
-  Definition key_gen   : nat := 42.
-  Definition KeyGen_locs := fset [:: pk_loc ; sk_loc ].
-  Definition KG_interface := [interface #val #[key_gen] : 'seed → 'pubkey ].
-
-  Definition KG_real :
-  package KeyGen_locs
-    [interface]
-    KG_interface
-  :=
-  [package
-    #def  #[key_gen] (sd : 'seed) : 'pubkey
-    {
-    '(sk,pk) ← KeyGenM sd ;;
-    ret pk
-    }
-  ].
-
-  Definition KG_ideal :
-  package KeyGen_locs
-    [interface]
-    KG_interface
-  :=
-  [package
-    #def  #[key_gen] (sd : 'seed) : 'pubkey
-    {
-    '(sk,pk) ← KeyGenM sd ;;
-    ret pk
-    }
-  ].
-
-  Definition mkpair {Lt Lf E}
-    (t: package Lt [interface] E)
-    (f: package Lf [interface] E):
-    loc_GamePair E :=
-    fun b => if b then {locpackage t} else {locpackage f}.
-
-  Definition KG_ind := @mkpair KeyGen_locs KeyGen_locs KG_interface KG_real KG_ideal.
-
-  Lemma KG_real_vs_KG_true :
-  KG_ind true ≈₀ KG_ind false.
-  Proof.
-    eapply eq_rel_perf_ind_eq.
-    simplify_eq_rel x.
-    all: ssprove_code_simpl.
-    Fail eapply rsame_head.
-    Fail eapply rsame_head_alt.
-    eapply rpost_weaken_rule.
-    1:{ eapply rreflexivity_rule. }
-    intros.
-    move: H1; case eq_a0: a₀ => [b₀ s₀]; move => eq_a1 //=.
-    move: eq_a1; case eq_a1: a₁ => [b₁ s₁]; move => eq_b_s.
-    by [injection eq_b_s => eq_s eq_b].
-  Qed.
-
-End RemoteAttestationM.
