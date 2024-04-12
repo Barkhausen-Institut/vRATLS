97694755ee23648f76aec7615040ee9a823a2d23
JaMaeh <jannik.maehn@barkhauseninstitut.org>
2024-04-12 17:12:23 +0200

    current version and deleted unnecessary files

---
+++ theories/examples/RSA.v
@@ -0,0 +1,44 @@
+From Relational Require Import OrderEnrichedCategory GenericRulesSimple.
+
+Set Warnings "-notation-overridden,-ambiguous-paths".
+From mathcomp Require Import all_ssreflect all_algebra reals distr realsum
+  fingroup.fingroup solvable.cyclic prime ssrnat ssreflect ssrfun ssrbool ssrnum
+  eqtype choice seq.
+Set Warnings "notation-overridden,ambiguous-paths".
+
+From mathcomp Require Import extra.
+
+From Crypt Require Import Axioms ChoiceAsOrd SubDistr Couplings
+  UniformDistrLemmas FreeProbProg Theta_dens RulesStateProb UniformStateProb
+  pkg_core_definition choice_type pkg_composition pkg_rhl
+  Package Prelude RandomOracle.
+
+From Coq Require Import Utf8.
+From extructures Require Import ord fset fmap.
+
+From Equations Require Import Equations.
+Require Equations.Prop.DepElim.
+Require Import Coq.Init.Logic.
+Require Import List.
+
+Set Equations With UIP.
+(*
+  This is needed to make definitions with Equations transparent.
+  Otherwise they are opaque and code simplifications in the
+  proofs with [ssprove_code_simpl] do not resolve properly.
+ *)
+Set Equations Transparent.
+
+Set Bullet Behavior "Strict Subproofs".
+Set Default Goal Selector "!".
+Set Primitive Projections.
+
+Import Num.Def.
+Import Num.Theory.
+Import Order.POrderTheory.
+
+Import PackageNotation.
+
+Obligation Tactic := idtac.
+
+#[local] Open Scope package_scope.
