c27648dddbda539097938036f5e2b6ff5e307813
Sebastian Ertel <sebastian.ertel@gmail.com>
2024-04-15 09:32:20 +0200

    fixing ord compilation with a shorter version of the proof.

--- theories/extructurespp/ord.v
+++ theories/extructurespp/ord.v
@@ -21,35 +21,11 @@ Section Facts.
   Lemma ord_lt_false :
     (b < a)%ord = true -> (a < b)%ord = false.
   Proof.
-  (* TODO there must be a simpler version of thios proof!
-
-             see below:
-             move => h.
-             rewrite /Ord.lt.
-             apply/negP/negP/nandP.
-             left.
-             rewrite Ord.ltNge in h; exact: h.
-   *)
-    move => h2.
-    move/idP: h2 => h2.
-    rewrite Ord.lt_neqAle in h2.
-    move/andP: h2; case => a_neq_a' a_lt_a'.
-    case: Ord.ltgtP => //=.
     move => h.
-    rewrite Ord.leq_eqVlt in a_lt_a'.
-    move/orP: a_lt_a'. case.
-    - move/eqP => a_eq_a' => //=. rewrite a_eq_a' in a_neq_a'. move/eqP: a_neq_a' => //=.
-    - move => a'_lt_a //=.
-      have bla := a'_lt_a.
-      move/eqP/eqP: bla => bla; rewrite -bla.
-
-      apply Ord.ltW in h.
-      apply Ord.ltW in a'_lt_a.
-      have c:= conj h a'_lt_a.
-      move/andP: c => c.
-      have d := (Ord.anti_leq c).
-      rewrite d.
-      by [apply Ord.ltxx].
+    rewrite /Ord.lt.
+    apply/negP/negP/nandP.
+    left.
+    rewrite Ord.ltNge in h; exact: h.
   Qed.
 
   Lemma lt_antisym:
